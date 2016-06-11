5daac356825b9249814a6d5d5112ad28ef347dc9
kapils527 <kapils527@gmail.com>
2016-06-11 04:41:51 +0000

    request page

---
+++ request.js
@@ -0,0 +1,17 @@
+var http = require('http')
+var url = require('url')
+var server = http.createServer(function(req,res)
+{
+   res.writeHead(200)
+   var page = url.parse(req.url).pathname;
+   if(page=="/login"){
+       res.write("login successfull")
+   }
+    else
+    {
+        res.write("login fail")
+        }
+        res.end()
+})
+
+server.listen(1337)
